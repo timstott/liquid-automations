6c2c5c0110fe35d03cc5bb6c8346d87dbe5bc5ae
Timothy Stott <stott.timothy@gmail.com>
2017-08-08 17:24:43 +0100

    :key: Add KMS decrypt utility function

---
+++ src/kms.ts
@@ -0,0 +1,13 @@
+import * as KMS from 'aws-sdk/clients/kms';
+
+const client = new KMS();
+
+const decrypt = (blob: string) => {
+  const ciphertext = Buffer.from(blob, 'base64');
+  return client
+    .decrypt({ CiphertextBlob: ciphertext })
+    .promise()
+    .then(result => result.Plaintext.toString());
+};
+
+export { decrypt }
